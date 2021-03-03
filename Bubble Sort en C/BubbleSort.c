f95e339288fb2b83d8113d7c5c450b08ff9852cf
CrisEsqui <crisesqui908@gmail.com>
2021-03-03 12:07:00 -0600

    Bubble sort en lenguaje C

---
+++ Bubble Sort en C/BubbleSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void bubbleSort(int pArray[], int pSize)
+{
+    int i,j,aux;
+
+    for (i=0; i<pSize; i++)
+    {
+        for(j=0; j<pSize; j=j+1)
+        {
+            if (pArray[j] > pArray[j+1])
+            {
+                aux = pArray[j];
+                pArray[j] = pArray[j+1];
+                pArray[j+1] = aux;
+            }
+        }
+    }
+}
+
+void printArray(int pArray[], int pSize)
+{
+    int i;
+    for (i=0; i<pSize; i++)
+    {
+        printf("%i  ", pArray[i]);
+    }
+}
+
+
+int main()
+{
+    int size = 10;
+    int numbers[] = {4,5,50,23,45,100,-1,30, -100, 55};
+
+    bubbleSort(numbers, size);
+    printArray(numbers, size);
+
+
+    return 0;
+}
